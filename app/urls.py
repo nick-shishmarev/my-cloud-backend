bc46fc8db75cd0889fe0c3e9b67a4a0983fdfcb2
Nikolai Shishmarev <nicansh@gmail.com>
2026-07-08 20:10:21 +0300

    User processing added

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from rest_framework.authtoken import views as auth_views
-from .views import RegisterView, CustomObtainAuthToken
+from .views import RegisterView, CustomObtainAuthToken, UsersView, UsersRetrieveUpdateDestroyView
+
+
+class FilesByUserForAdminView:
+    pass
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('api-token-auth/', CustomObtainAuthToken.as_view(), name='api_token_auth'),
+    path('users/', UsersView.as_view(), name='users'),
+    path('users/<int:pk>', UsersRetrieveUpdateDestroyView.as_view(), name='users2'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
+from django.db.models import ProtectedError
 from rest_framework import generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, User, UsersSerializer
 
 
 # Регистрация нового пользователя
@@ -29,7 +30,7 @@ class RegisterView(generics.CreateAPIView):
         }, status=status.HTTP_201_CREATED)
 
 
-			
+# Получение токена и user при логине
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(
@@ -51,3 +52,30 @@ class CustomObtainAuthToken(ObtainAuthToken):
                 'is_staff': user.is_staff,
             },
         })
+
+
+# Список пользователей
+class UsersView(generics.ListAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
+
+
+# Обработка заданного пользователя (для админа)
+class UsersRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UsersSerializer
+    queryset = User.objects.all()
+
+    # Проверка наличия у пользователя файлов при удалении
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except ProtectedError as protected_error:
+            protected_elements = [
+                {"id": obj.pk, "label": str(obj)}
+                for obj in protected_error.protected_objects
+            ]
+            return Response(
+                data={"protected_elements": protected_elements},
+                status=status.HTTP_423_LOCKED
+            )
+
